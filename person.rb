1c45a61e5fa9661eda5cd355e04e401e6591e55e
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-15 16:58:23 +0300

    Resolve comments

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
@@ -6,14 +9,13 @@ class Person
     @parent_permission = parent_permission
   end
 
-  attr_reader :id
-  attr_accessor :name, :age
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
 
   def of_age?
     @age >= 18
-  end
-
-  def can_use_services?
-    is_of_age? || @parent_permission
   end
 end
