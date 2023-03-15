51f0aba12efcd20bb7321437593542ee237f798b
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-15 13:21:12 +0300

    Fix linters

--- person.rb
+++ person.rb
@@ -1,16 +1,18 @@
 class Person
-    def initialize(age, name = "Unknown", parent_permission = true)
-        @name = name
-        @age = age
-        @id = id
-        @parent_permission = parent_permission
-    end
-    attr_accessor :id,:name,:age
-    attr_writer :name, :age
-    def is_of_age?
-        @age >= 18
-    end
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @name = name
+    @age = age
+    @id = id
+    @parent_permission = parent_permission
+  end
+  attr_accessor :id, :name, :age
+  attr_writer :name, :age
+
+  def is_of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 end
