cce80596f319f244ef8577205a65cd0e6acdb6c8
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-17 23:39:46 +0300

    Create main class as an entry point

---
+++ main.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+require_relative './app.rb'
+class Main
+    def initialize
+        @app = App.new
+    end
+
+    def start
+        puts 'You have started the school library app!'
+        puts 
+        loop do 
+            show_menu
+            option = gets.chomp.to_i
+            if option == 7
+                puts 'Thank you for using this app' 
+                break
+            end
+            choose_option(option)
+        end
+    end
+    
+    def show_menu
+        puts 'Please select an option by entering a number:'
+        puts '1 - List all people'
+        puts '2 - List all books'
+        puts '3 - Create a person'
+        puts '4 - Create a book'
+        puts '5 - Create a rental'
+        puts '6 - List all rentals for a given person id'
+        puts '7 - Exit'
+        print 'Enter your option: '
+    end
+
+    def choose_option(option)
+        case option
+        when 1
+            @app.list_people
+        when 2
+            @app.list_books
+        when 3
+            @app.create_person
+        when 4
+            @app.create_book
+        when 5
+            @app.create_rental
+        when 6
+            @app.list_rentals
+        else
+            puts 'Invalid option'
+        end
+    end
+end
+main = Main.new
+main.start
